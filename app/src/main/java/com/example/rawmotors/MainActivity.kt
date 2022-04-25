cda857a6f856f0f006eb9e06f0aa0cd52bdd8a74
edupovea <edu.povea15@gmail.com>
2022-04-25 18:50:22 +0200

    login and registro completado

--- app/src/main/java/com/example/rawmotors/MainActivity.kt
+++ app/src/main/java/com/example/rawmotors/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.rawmotors
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    val btnContinue: Button by lazy {
+        findViewById<Button>(R.id.btnContinue)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnContinue.setOnClickListener {
+            val intent: Intent =
+                Intent(
+                    this@MainActivity,
+                    LoginActivity::class.java
+                )
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/rawmotors/RegistroActivity.kt
+++ app/src/main/java/com/example/rawmotors/RegistroActivity.kt
@@ -2,10 +2,85 @@ package com.example.rawmotors
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.*
 
 class RegistroActivity : AppCompatActivity() {
+
+    val email: EditText by lazy { findViewById(R.id.txtEmailReg) }
+    val user : EditText by lazy { findViewById(R.id.txtUserReg)}
+    val pass : EditText by lazy { findViewById(R.id.txtPassReg)}
+
+    val auth by lazy { FirebaseAuth.getInstance() }
+
+    val btnConfirm: Button by lazy {
+        findViewById<Button>(R.id.btnConfirm)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registro)
+
+        btnConfirm.setOnClickListener {
+            if (!email.text.isBlank() && !pass.text.isBlank() && !user.text.isBlank()) {
+                val tarea = auth.createUserWithEmailAndPassword(
+                    email.text.toString(), pass.text.toString()
+                )
+
+                tarea.addOnCompleteListener ( this ,
+                    object : OnCompleteListener<AuthResult>{
+                        override fun onComplete(p0: Task<AuthResult>) {
+                            if (tarea.isSuccessful){
+                                Toast.makeText(
+                                    this@RegistroActivity,
+                                    getString(R.string.registroCompletado)
+                                            + " " + auth.currentUser?.email.toString(),
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            }else{
+                                try {
+                                    throw tarea.exception!!
+                                } catch (e: FirebaseAuthWeakPasswordException) {
+                                    Toast.makeText(
+                                        this@RegistroActivity,
+                                        R.string.contraseñaDebil,
+                                        Toast.LENGTH_SHORT
+                                    ).show()
+                                } catch (e: FirebaseAuthInvalidCredentialsException) {
+                                    Toast.makeText(
+                                        this@RegistroActivity,
+                                        R.string.credencialesInvalidos,
+                                        Toast.LENGTH_SHORT
+                                    ).show()
+                                } catch (e: FirebaseAuthUserCollisionException) {
+                                    Toast.makeText(
+                                        this@RegistroActivity,
+                                        R.string.colisionUsuario,
+                                        Toast.LENGTH_SHORT
+                                    ).show()
+                                } catch (e: Exception) {
+                                    Toast.makeText(
+                                        this@RegistroActivity,
+                                        R.string.errorDesconocido,
+                                        Toast.LENGTH_SHORT
+                                    ).show()
+                                }
+                            }
+                        }
+
+
+                    })                    }else{
+                Toast.makeText(
+                    this@RegistroActivity,R.string.camposRegistroNoRellenos,
+                    Toast.LENGTH_LONG
+                ).show()
+
+            }
+            }
+        }
+
+
     }
-}
