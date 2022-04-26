6cf524e6440e4c290eb9dc02707759c887f90cee
edupovea <edu.povea15@gmail.com>
2022-04-26 18:14:59 +0200

    registro con coleccion + strings

--- app/src/main/java/com/example/rawmotors/RegistroActivity.kt
+++ app/src/main/java/com/example/rawmotors/RegistroActivity.kt
@@ -8,8 +8,11 @@ import android.widget.Toast
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.*
+import com.google.firebase.firestore.FirebaseFirestore
 
 class RegistroActivity : AppCompatActivity() {
+
+    val firestore by lazy { FirebaseFirestore.getInstance() }
 
     val email: EditText by lazy { findViewById(R.id.txtEmailReg) }
     val user : EditText by lazy { findViewById(R.id.txtUserReg)}
@@ -29,6 +32,24 @@ class RegistroActivity : AppCompatActivity() {
                 val tarea = auth.createUserWithEmailAndPassword(
                     email.text.toString(), pass.text.toString()
                 )
+
+                val valoresLogin: HashMap<String, Any> =
+                    HashMap<String, Any>()
+                valoresLogin.put("email" , email.text.toString())
+
+                val tareaInsertar= firestore.collection("users").document(
+                    user.text.toString()).set(valoresLogin)
+
+                tareaInsertar.addOnCompleteListener {
+                    if (it.isSuccessful){
+                        Toast.makeText(this@RegistroActivity, "coleccion insertada", Toast.LENGTH_SHORT).show()
+                    }else{
+                        Toast.makeText(this@RegistroActivity, "coleccion NO insertada PUTA", Toast.LENGTH_SHORT).show()
+
+                    }
+                }
+
+
 
                 tarea.addOnCompleteListener ( this ,
                     object : OnCompleteListener<AuthResult>{
@@ -72,7 +93,8 @@ class RegistroActivity : AppCompatActivity() {
                         }
 
 
-                    })                    }else{
+                    })
+            }else{
                 Toast.makeText(
                     this@RegistroActivity,R.string.camposRegistroNoRellenos,
                     Toast.LENGTH_LONG
